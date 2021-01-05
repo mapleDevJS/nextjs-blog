6545a146bcb9d2738389921d029e3a5819820357
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2021-01-05 11:17:58 -0500

    feat: Render Markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,16 +2,18 @@ import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
-  return (
-    <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-    </Layout>
-  )
-}
+    return (
+      <Layout>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </Layout>
+    )
+  }
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
@@ -22,7 +24,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
